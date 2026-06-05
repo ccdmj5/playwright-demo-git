0eecbcc0d0028b1e8640ca2fea6f153b3173071a
ccdmj5 <josue_fonseca_1499@hotmail.com>
2026-06-04 17:58:10 -0700

    Quick fix

--- tests/test_Practicetestautomation.spec.ts
+++ tests/test_Practicetestautomation.spec.ts
@@ -2,12 +2,11 @@ import { test, expect } from '@playwright/test';
 import { POM_Model } from './pOM_Model';
 
 test('practiceTestautomation', async ({ page }) => {
-    const username = 'student';
-    const password = 'Password123';
     
     const pom = new POM_Model(page);
+    
     await pom.goto();
-    await pom.login(username, password);
+    await pom.login('student', 'Password123');
     await pom.verifyLoginSuccess();
     await pom.logout();
 });
